0f3c6013497231c20e6f2fffdb4a67db92637370
pp-reema <reemapp88@gmail.com>
2025-04-17 14:55:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title=" Personality Interview", layout="centered",, initial_sidebar_state="collapsed")
+st.set_page_config(page_title=" Personality Interview", layout="centered", initial_sidebar_state="collapsed")
 
 # Redirect to Welcome page
 st.switch_page("pages/1_Welcome.py")
